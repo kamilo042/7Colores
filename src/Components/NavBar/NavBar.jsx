1b5987f21e0854c717eb9b4f97df9590bfb68f9a
kamilo042 <kamilo042.cp@gmail.com>
2024-04-29 23:46:12 -0500

    icono de cart agregado

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,27 @@
+import logo from '../../assets/Logo 7colores-02.png';
+import cartIcon from '../../assets/cart.svg'; // Assuming you have an SVG named cart.svg
+
+const Navbar = () => {
+  return (
+    <nav className="grid grid-cols-1 place-items-center py-4 bg-gray-100 relative">
+      <img src={logo} alt="Company Logo" className="w-100 h-20 mb-4" />
+      <ul className="flex space-x-6">
+        <li><a href="#" className="text-gray-700 hover:text-gray-900">Inicio</a></li>
+        <li><a href="#" className="text-gray-700 hover:text-gray-900">Catálogo</a></li>
+        <li><a href="#" className="text-gray-700 hover:text-gray-900">Galería</a></li>
+        <li><a href="#" className="text-gray-700 hover:text-gray-900">Sobre Nosotros</a></li>
+        <li><a href="#" className="text-gray-700 hover:text-gray-900">Blog/Noticias</a></li>
+        <li><a href="#" className="text-gray-700 hover:text-gray-900">Contacto</a></li>
+        <li><a href="#" className="text-gray-700 hover:text-gray-900">FAQ/Preguntas frecuentes</a></li>
+        <li>
+          <a href="#" className="text-gray-700 hover:text-gray-900">Carrito</a>
+        </li>
+        <li><a href="#" className="text-gray-700 hover:text-gray-900">Buscar</a></li>
+        <li><a href="#" className="text-gray-700 hover:text-gray-900">Redes Sociales</a></li>
+      </ul>
+      <img src={cartIcon} alt="Cart Icon" className="w-6 h-6 absolute top-4 right-4" />
+    </nav>
+  );
+};
+
+export default Navbar;

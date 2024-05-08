e3e247aaa4f98be1d62023a4d5609b3dc5b835f4
CamiloDev22 <kamilo042.cp@gmail.com>
2024-05-08 15:26:43 -0500

    banner añadido

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import './App.css'
-import CategoriesSection from './Components/CategoriesSection/CategoriesSection'
-import FooterEl from './Components/Footer/Footer'
-import Gallery from './Components/Gallery/Gallery'
-import NavBar from './Components/NavBar/NavBar'
-import NewCards from './Components/NewCards/NewCards'
+import './App.css';
+import CategoriesSection from './Components/CategoriesSection/CategoriesSection';
+import FooterEl from './Components/Footer/Footer';
+import Gallery from './Components/Gallery/Gallery';
+import NavBar from './Components/NavBar/NavBar';
+import NewCards from './Components/NewCards/NewCards';
+import Banner from './Components/Banner/Banner';
 
 function App() {
-  
-
   return (
     <>
-    <NavBar/>
-    <CategoriesSection/>
-    <h1 className="text-center text-4xl font-sans pb-3  pt-5">NUEVOS DISEÑOS</h1>
-     <NewCards/> 
-     <h1 className="text-center text-4xl font-sans pb-3  pt-5">Galeria</h1>
-    <Gallery/>
-    <FooterEl/>
+      <NavBar />
+      <CategoriesSection />
+      <h1 className="nordic-h1">NUEVOS DISEÑOS</h1>
+      <NewCards />
+      <Banner />
+      <h1 className="nordic-h1">Galeria</h1>
+      <Gallery />
+      <FooterEl />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

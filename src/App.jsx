8d26b6c9f85766a0ade9282ed9822c6593aa089a
CamiloDev22 <kamilo042.cp@gmail.com>
2024-05-08 01:33:27 -0500

    galeria

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import CategoriesSection from './Components/CategoriesSection/CategoriesSection'
 import FooterEl from './Components/Footer/Footer'
+import Gallery from './Components/Gallery/Gallery'
 import NavBar from './Components/NavBar/NavBar'
 import NewCards from './Components/NewCards/NewCards'
 
@@ -13,6 +14,8 @@ function App() {
     <CategoriesSection/>
     <h1 className="text-center text-4xl font-sans pb-3  pt-5">NUEVOS DISEÑOS</h1>
      <NewCards/> 
+     <h1 className="text-center text-4xl font-sans pb-3  pt-5">Galeria</h1>
+    <Gallery/>
     <FooterEl/>
     </>
   )
